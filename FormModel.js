41242e0845911aaa0489196b6de63193c68ea40f
dirkliu <liuqi0702@126.com>
2018-12-23 23:01:45 +0800

    init

---
+++ FormModel.js
@@ -0,0 +1,39 @@
+class FormModel {
+
+  constructor (options) {
+    this._observeKeys = []
+    this.defaults = {}
+    Object.assign(this, options)
+    this._setObserve(options)
+    return this
+  }
+
+  _setObserve (options) {
+    Object.keys(options).forEach(key => {
+      if (this._observeKeys.indexOf(key) === -1) {
+        this._observeKeys.push(key)
+        Object.defineProperty(this, key, {
+          set (value) {
+            this['_' + key] = value || options[key]
+          },
+          get () {
+          // console.log('key:', key)
+            return this['_' + key] || options[key]
+          }
+      })
+      }
+    })
+  }
+
+  setDefaults (options) {
+    // Object.assign(this, options)
+    Object.keys(options).forEach(key => {
+      Object.assign(this, {
+        key: this[key] || options[key]
+      })
+    })
+    this._setObserve(options)
+    Object.assign(this.defaults, options)
+    return this
+  }
+}
